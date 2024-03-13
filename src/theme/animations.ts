ad0d08c88a4908e6eee926011add2d7efdb58fb1
Josh Murr <joshuadavidmurr@gmail.com>
2024-03-13 17:08:41 +0000

    Not using conditional thing

--- src/theme/animations.ts
+++ src/theme/animations.ts
@@ -1,9 +1,3 @@
-export const conditionalEnterExit = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1 },
-  exit: (val) => ({ opacity: val ? 0 : 1 }),
-};
-
 export const transition = {
   duration: 1,
 };
