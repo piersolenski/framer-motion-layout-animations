b038ef56563ab2619f1422cd411feb85713bf625
Josh Murr <joshuadavidmurr@gmail.com>
2024-03-15 12:05:36 +0000

    React player component + install

---
+++ src/components/Video/icons/FullscreenOpen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export function FullscreenOpen() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 -960 960 960"
+    >
+      <path
+        fill="currentColor"
+        d="M240-120v-120H120v-80h200v200h-80zm400 0v-200h200v80H720v120h-80zM120-640v-80h120v-120h80v200H120zm520 0v-200h80v120h120v80H640z"
+      />
+    </svg>
+  );
+}
